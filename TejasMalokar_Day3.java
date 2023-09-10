cb55610ff567edf5abf0f9f3d9f5edc805824dc1
Tejasmalokar2002 <tejasmalokar707@gmail.com>
2023-09-10 21:50:11 +0530

    Uploaded

---
+++ TejasMalokar_Day3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Day3
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ascii = sc.next().charAt(0);
+        System.out.println((int)ascii);
+    }
+}
---
+++ TejasMalokar_Day4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Day4{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a<0){
+            System.out.println("Positive number");
+        } else{
+            System.out.println("Negative number");
+        }
+    }
+}
